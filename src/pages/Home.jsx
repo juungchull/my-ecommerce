0cb0da4751362567bad648f0572761eeb199f82d
juungchull <89003952+juungchull@users.noreply.github.com>
2023-02-06 14:04:46 +0900

    popular

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,10 @@ import Clock from "../UI/Clock";
 const Home = () => {
   const [trendingProducts, setTrendingProducts] = useState([]);
   const [bestSalesProducts, setBestSalesProducts] = useState([]);
+  const [mobileProducts, setMobileProducts] = useState([]);
+  const [wirelessProducts, setWirelessProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
+
   const year = new Date().getFullYear();
 
   useEffect(() => {
@@ -26,19 +30,31 @@ const Home = () => {
     const filteredBestSalesProducts = products.filter(
       (item) => item.category === "sofa"
     );
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === "mobile"
+    );
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === "wireless"
+    );
+    const filteredPopularProducts = products.filter(
+      (item) => item.category === "popular"
+    );
 
     setTrendingProducts(filteredTrendingProducts);
     setBestSalesProducts(filteredBestSalesProducts);
+    setMobileProducts(filteredMobileProducts);
+    setWirelessProducts(filteredWirelessProducts);
+    setPopularProducts(filteredPopularProducts);
   }, []);
 
   return (
     <Helmet title={"Home"}>
-      <section className='hero__section'>
+      <section className="hero__section">
         <Container>
           <Row>
-            <Col lg='6' md='6'>
-              <div className='hero__content'>
-                <p className='hero__subtitle'>Trending product in {year}</p>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
                 <h2>Make Your Interior More Minimalistic & Modern</h2>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
@@ -46,64 +62,83 @@ const Home = () => {
                   molestias et.
                 </p>
 
-                <motion.button whileTap={{ scale: 1.2 }} className='shop__btn'>
-                  <Link to='/shop'>SHOP NOW</Link>
+                <motion.button whileTap={{ scale: 1.2 }} className="shop__btn">
+                  <Link to="/shop">SHOP NOW</Link>
                 </motion.button>
               </div>
             </Col>
-            <Col lg='6' md='6'>
-              <div className='hero__img'>
-                <img src={heroImage} alt='' />
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImage} alt="" />
               </div>
             </Col>
           </Row>
         </Container>
       </section>
       <Services />
-      <section className='trending__products'>
+      <section className="trending__products">
         <Container>
           <Row>
-            <Col lg='12' className='text-center'>
-              <h2 className='section__title'>Trending Proudcts</h2>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Trending Proudcts</h2>
             </Col>
             <ProductsList data={trendingProducts} />
           </Row>
         </Container>
       </section>
 
-      <section className='best__sales'>
+      <section className="best__sales">
         <Container>
           <Row>
-            <Col lg='12' className='text-center'>
-              <h2 className='section__title'>Best Sales</h2>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best Sales</h2>
             </Col>
             <ProductsList data={bestSalesProducts} />
           </Row>
         </Container>
       </section>
 
-      <section className='timer__count'>
+      <section className="timer__count">
         <Container>
           <Row>
-            <Col lg='6' md='6'>
-              <div className='clock__top-content'>
-                <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
-                <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
               </div>
               <Clock />
 
               <motion.button
                 whileTap={{ scale: 1.2 }}
-                className='buy__btn store__btn'
+                className="buy__btn store__btn"
               >
-                <Link to='/shop'>Visit Store</Link>
+                <Link to="/shop">Visit Store</Link>
               </motion.button>
             </Col>
-            <Col lg='6' md='6' className='text-end'>
-              <img src={counterImg} alt='' />
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="" />
             </Col>
           </Row>
         </Container>
+      </section>
+
+      <section className="new__arrivals">
+        <Row>
+          <Col text="12" className="text-center">
+            <h2 className="section__title">New Arrivals</h2>
+          </Col>
+          <ProductsList data={mobileProducts} />
+          <ProductsList data={wirelessProducts} />
+        </Row>
+      </section>
+
+      <section className="popular__category">
+        <Row>
+          <Col text="12" className="text-center">
+            <h2 className="section__title">Popular in Category</h2>
+          </Col>
+          <ProductsList data={popularProducts} />
+        </Row>
       </section>
     </Helmet>
   );
